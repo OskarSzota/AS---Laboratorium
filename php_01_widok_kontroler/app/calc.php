c4b0d762042b7daedfb20f315e0aa8d0bb1c902c
OskarSzota <oskar.szota123@interia.pl>
2024-11-29 12:34:12 +0100

    Add files via upload

---
+++ php_01_widok_kontroler/app/calc.php
@@ -0,0 +1,68 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$kwota = $_REQUEST ['kwota'];
+$miesiac = $_REQUEST ['miesiac'];
+$oprocentowanie = $_REQUEST ['oprocentowanie'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($kwota) && isset($miesiac) && isset($oprocentowanie))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $kwota == "") {
+	$messages [] = 'Nie podano kwoty kredytu';
+}
+if ( $miesiac == "") {
+	$messages [] = 'Nie podano liczby miesiecy';
+}
+if ( $oprocentowanie == "") {
+	$messages [] = 'Nie podano oprocentowania';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Kwota nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $miesiac )) {
+		$messages [] = 'Liczba miesięcy nie jest liczbą całkowitą';
+	}	
+
+	if (! is_numeric( $oprocentowanie )) {
+		$messages [] = 'Oprocentowanie nie jest liczbą całkowitą';
+	}
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int
+	$kwota = intval($kwota);
+	$miesiac = intval($miesiac);
+	$oprocentowanie = intval($oprocentowanie)/100;
+	$odsetki = ($kwota * (($miesiac/12)*$oprocentowanie));
+	$result = ($kwota+$odsetki)/$miesiac;
+
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
